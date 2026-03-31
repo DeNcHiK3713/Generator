92bf12559e2065fc623b571f1da13af4005f2b44
Denis Suchok <den6281505@gmail.com>
2026-03-31 13:37:02 +0300

    Added architecture detection from file

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -9,9 +9,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-if (args.Length < 4 || !File.Exists(args[0]) || !File.Exists(args[1]) || !File.Exists(args[2]) || !Enum.TryParse<Architecture>(args[3], out var arch))
+if (args.Length < 3 || !File.Exists(args[0]) || !File.Exists(args[1]) || !File.Exists(args[2]))
 {
-    Console.WriteLine("Usage: generator template.json script.json libil2cpp.so arch");
+    Console.WriteLine("Usage: generator template.json script.json libil2cpp.so");
     return;
 }
 var lines = JsonConvert.DeserializeObject<IEnumerable<ILine>>(File.ReadAllText(args[0]), new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
@@ -19,6 +19,17 @@ var scriptJson = JsonConvert.DeserializeObject<ScriptJson>(File.ReadAllText(args
 
 using (var il2cpp = File.OpenRead(args[2]))
 {
+    var pos = il2cpp.Position;
+    using var elf = ELFReader.Load(il2cpp, false);
+    var machine = elf.Machine;
+    il2cpp.Position = pos;
+
+    var arch = machine switch {
+        Machine.ARM => Architecture.ARM,
+        Machine.AArch64 => Architecture.ARM64,
+        _ => throw new NotSupportedException()
+    };
+
     var addressConverter = new Il2CppAddressConverter(il2cpp, arch);
     lines.OfType<PatchLine>().ForEach(x => x.FindPatch(scriptJson, il2cpp, arch, addressConverter));
 }
--- Generator/generate.bat
+++ Generator/generate.bat
@@ -1,8 +1,8 @@
 @echo off
 echo #ifdef __arm__
-generator template.json .\path\to\script.json .\path\to\libil2cpp.so ARM
+generator template.json .\path\to\script.json .\path\to\libil2cpp.so
 echo #elif __aarch64__
 echo.
-generator template.json .\path\to\script.json .\path\to\libil2cpp.so ARM64
+generator template.json .\path\to\script.json .\path\to\libil2cpp.so
 echo #endif
 pause>nul
